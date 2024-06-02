c03f63af447ca0f90a9693734a9d0ec85522ad6c
AndriyBorkovich <borkovich25andri@gmail.com>
2024-06-02 10:54:12 +0300

    refactor controllers

--- FootballManager.API/Controllers/AccountController.cs
+++ FootballManager.API/Controllers/AccountController.cs
@@ -8,27 +8,30 @@ namespace FootballManager.API.Controllers;
 /// <inheritdoc />
 [Route("api/[controller]")]
 [ApiController]
-public class AccountController : ControllerBase
+public class AccountController(IAccountService accountService) : ControllerBase
 {
-    private readonly IAccountService _accountService;
-
-    public AccountController(IAccountService accountService)
-    {
-        _accountService = accountService;
-    }
-
+    /// <summary>
+    /// Register a new user
+    /// </summary>
+    /// <param name="request">New user's data: first and last names, email and password</param>
+    /// <returns>Result of the operation, and errors (if exist)</returns>
     [HttpPost("Register")]
     public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest request)
     {
-        var result = await _accountService.Register(request);
+        var result = await accountService.Register(request);
 
         return this.FromResult(result);
     }
 
+    /// <summary>
+    /// Login with existing user's credentials
+    /// </summary>
+    /// <param name="request">Contains username and password</param>
+    /// <returns>Result of the operation, JWT or error message</returns>
     [HttpPost("Login")]
     public async Task<ActionResult<AuthResponse>> Login(AuthRequest request)
     {
-        var result = await _accountService.Login(request);
+        var result = await accountService.Login(request);
 
         return this.FromResult(result);
     }
